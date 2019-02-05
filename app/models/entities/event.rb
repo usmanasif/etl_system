c61ca4aee0a06e906a7fd598d094dea8662d99f0
Usman Asif <usmanasif@users.noreply.github.com>
2019-02-04 16:06:53 -0800

    Entities::Event: Implemented mapping

---
+++ app/models/entities/event.rb
@@ -0,0 +1,30 @@
+module Entities
+  class Event < Entity
+    class << self
+      def internal_entity_name
+        'Appointment'
+      end
+
+      def external_entity_name
+        'Event'
+      end
+
+      def mapper
+        EventMapper
+      end
+    end
+  end
+end
+
+class EventMapper
+  extend HashMapper
+
+  map from('id'), to('reference_id')
+  map from('start_time'), to('begin_at')
+  map from('end_time'), to('end_at')
+
+  after_normalize do |input, output|
+    output['client_id'] = Client.find_by(reference_id: input['customer_id'])&.id
+    output
+  end
+end
--- app/services/mapping/base.rb
+++ app/services/mapping/base.rb
@@ -3,7 +3,7 @@ require 'csv'
 module Mapping
   POSSIBLE_CSV_DEFINITIONS = {
     csv_mapping_1: %w(services.csv clients.csv appointments.csv appointment_services.csv),
-    csv_mapping_2: %w(variations.csv customers.csv)
+    csv_mapping_2: %w(variations.csv customers.csv events.csv)
   }.with_indifferent_access
 
   class Base
